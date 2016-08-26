331b70e27fcc24043b98297f5a8a7a5ce7bb80e8
Nicolay Izoderov <nico-izo@ya.ru>
2016-08-26 16:04:50 +0300

    Minor changes in setup.py

--- setup.py
+++ setup.py
@@ -3,29 +3,14 @@ from codecs import open
 from os import path
 from warnings import warn
 
-def readme():
-    here = path.abspath(path.dirname(__file__))
-    readme = path.join(here, 'README.md')
-    
-    try:
-        import pypandoc
-        long_description = pypandoc.convert_file(readme, 'rst', format='md')
-    except ImportError:
-        warn("Markdown -> Rst convestion failed")
-        with open(readme, encoding='utf-8') as f:
-            long_description = f.read()
-            
-    return long_description
-
 setup(name='kobato',
       version='0.1',
       description='A command line interface to your point.im blog',
-      long_description=readme(),
       url='https://github.com/nico-izo/kobato',
       author='Nicolay Izoderov',
       author_email='nico-izo@ya.ru',
       license='GNU/GPLv3',
-      packages=['kobato'],
+      packages=['kobato', 'kobato.commands'],
       install_requires=[
           'decorating',
           'appdirs',
@@ -39,9 +24,6 @@ setup(name='kobato',
         'Topic :: Utilities'
       ],
       keywords='social blog cli',
-      extras_require={
-        "Requires-Dist": ["pypandoc"]
-      },
       entry_points = {
         'console_scripts': ['kobato=kobato.command_line:main'],
       },
