e6bd10d324c2b91d435dda73a4ac2cfba1d8dd86
Nicolay Izoderov <nico-izo@ya.ru>
2016-08-29 18:54:12 +0300

    moved version to __init__.py, added python3_only tag, updated license

---
+++ kobato/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1'
+__author__ = 'Nicolay Izoderov'
+__email__ = 'nico-izo@ya.ru'
+__url__ = 'https://github.com/nico-izo/kobato'
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
+import kobato
 
-setup(name='kobato',
-      version='0.1',
+setup(name=kobato.__name__,
+      version=kobato.__version__,
       description='A command line interface to your point.im blog',
-      url='https://github.com/nico-izo/kobato',
-      download_url = 'https://github.com/nico-izo/kobato/tarball/0.1',
-      author='Nicolay Izoderov',
-      author_email='nico-izo@ya.ru',
-      license='GNU/GPLv3',
+      url=kobato.__url__,
+      download_url = '{url}/tarball/{version}'.format(url = kobato.__url__, version = kobato.__version__),
+      author=kobato.__author__,
+      author_email=kobato.__email__,
+      license='GNU/GPLv3+',
       packages=['kobato', 'kobato.commands'],
       install_requires=[
           'decorating',
@@ -15,10 +16,12 @@ setup(name='kobato',
           'requests',
           'pyyaml'
       ],
+      # https://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet',
         'Topic :: Utilities'
       ],
